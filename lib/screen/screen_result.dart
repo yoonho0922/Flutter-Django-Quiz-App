2bd6ebbe5d5d16d00fb1eabd69fa69e166f74faf
yoonho <yoonho0922@naver.com>
2021-05-15 11:11:41 +0900

    feat: result page impl

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -40,7 +40,80 @@ class ResultScreen extends StatelessWidget {
               width: width * 0.85,
               height: height * 0.5,
               child: Column(
-                children: <Widget>[],
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: width * 0.048),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.deepPurple),
+                      color: Colors.white,
+                    ),
+                    width: width * 0.73,
+                    height: height * 0.25,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(
+                              0, width * 0.048, 0, width * 0.012),
+                          child: Text(
+                            '수고하셨습니다!',
+                            style: TextStyle(
+                              fontSize: width * 0.055,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '당신의 점수는',
+                          style: TextStyle(
+                            fontSize: width * 0.048,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Text(
+                          score.toString() + '/' + quizs.length.toString(),
+                          style: TextStyle(
+                            fontSize: width * 0.21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(width * 0.012),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: width * 0.048),
+                    child: ButtonTheme(
+                      minWidth: width * 0.73,
+                      height: height * 0.05,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomeScreen();
+                          }));
+                        },
+                        child: Text('홈으로 돌아가기'),
+                        color: Colors.white,
+                        textColor: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
